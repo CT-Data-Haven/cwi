3aba45f966218adffe7030b8cfb463ecd7912a33
camille-s <camille.seaberry@gmail.com>
2018-07-02 14:05:17 -0400

    Setup tibble of MSAs in New England as data obj

---
+++ data-raw/make_msas.R
@@ -0,0 +1,15 @@
+# make list of regional MSAs via tidycensus
+fips <- tibble::tibble(
+  state = c("CT", "MA", "ME", "NH", "RI", "VT"),
+  code = c("09", "23", "25", "33", "44", "50")
+)
+
+msa <- fips$code %>%
+  purrr::map_dfr(~tidycensus::get_acs(geography = "metropolitan statistical area/micropolitan statistical area", table = "B01003", year = 2016, state = .) %>%
+                   dplyr::select(GEOID, name = NAME) %>%
+                   dplyr::filter(stringr::str_detect(name, "Metro Area"))) %>%
+  dplyr::mutate(GEOID = stringr::str_sub(GEOID, 3, -1)) %>%
+  dplyr::mutate(name = stringr::str_remove(name, " Metro Area.+$")) %>%
+  unique()
+
+usethis::use_data(msa)
