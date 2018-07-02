91bd324fe71a705d38d8a99d7d5414ed1f1adf55
camille-s <camille.seaberry@gmail.com>
2018-07-02 16:51:11 -0400

    Change gray colors; add title param

--- R/geo_level_plot.R
+++ R/geo_level_plot.R
@@ -7,16 +7,17 @@
 #' @param level Bare column name containing geographic levels for fill.
 #' @param type String: one of `"col"` (bar chart), `"point"` (dot plot), or `"hist"` (histogram); defaults `"col"`.
 #' @param hilite String giving the highlight color, used for the lowest geography present.
-#' @param min_gray String giving the named gray color for the highest geography; defaults `"gray20"`.
-#' @param max_gray String giving the named gray color for the second lowest geography; defaults `"gray60"`.
+#' @param title String giving the title, if desired, for the plot.
+#' @param dark_gray String giving the named gray color for the highest geography; defaults `"gray20"`.
+#' @param light_gray String giving the named gray color for the second lowest geography; defaults `"gray60"`.
 #' @param ... Any additional parameters to pass to the underlying geom function.
 #' @return A ggplot
 #' @export
-geo_level_plot <- function(data, name = name, value = value, level = level, type = "col", hilite = "dodgerblue", min_gray = "gray20", max_gray = "gray60", ...) {
+geo_level_plot <- function(data, name = name, value = value, level = level, type = "col", hilite = "dodgerblue", title = NULL, dark_gray = "gray20", light_gray = "gray60", ...) {
   # type can be column (col), histogram (hist), or point (point)
   if (!type %in% c("col", "hist", "point")) stop("'type' must be one of col, hist, or point")
-  if (!grepl("gr(a|e)y", min_gray)) min_gray <- "gray20"
-  if (!grepl("gr(a|e)y", max_gray)) max_gray <- "gray60"
+  if (!grepl("gr(a|e)y", dark_gray)) dark_gray <- "gray20"
+  if (!grepl("gr(a|e)y", light_gray)) light_gray <- "gray60"
 
   value_var <- rlang::enquo(value)
   name_var <- rlang::enquo(name)
@@ -24,8 +25,8 @@ geo_level_plot <- function(data, name = name, value = value, level = level, type
 
   geos <- data %>% dplyr::pull(level) %>% unique() %>% sort()
 
-  g1 <- stringr::str_extract(min_gray, "\\d+$") %>% as.numeric()
-  g2 <- stringr::str_extract(max_gray, "\\d+$") %>% as.numeric()
+  g1 <- stringr::str_extract(dark_gray, "\\d+$") %>% as.numeric()
+  g2 <- stringr::str_extract(light_gray, "\\d+$") %>% as.numeric()
   pal1 <- round(seq(g1, g2, length.out = length(geos) - 1))
   pal <- c(paste0("gray", pal1), hilite)
 
@@ -46,6 +47,9 @@ geo_level_plot <- function(data, name = name, value = value, level = level, type
     p_out <- p +
       ggplot2::geom_col(ggplot2::aes(x = !!name_var, y = !!value_var), width = 0.8, ...) +
       ggplot2::coord_flip()
+  }
+  if (!is.null(title)) {
+    p_out <- p_out + ggplot2::ggtitle(title)
   }
   p_out + ggplot2::theme_minimal()
 }
