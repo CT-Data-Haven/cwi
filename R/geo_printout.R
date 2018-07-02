1c3c7da444fb8fcbfa3a04875b31bcc6b8c3188b
camille-s <camille.seaberry@gmail.com>
2018-07-02 17:31:05 -0400

    Add geo_printout

---
+++ R/geo_printout.R
@@ -0,0 +1,20 @@
+# make geography printout--not exported
+geo_printout <- function(neighborhoods, towns, regions, counties, state, msa, us) {
+
+  out <- list(names(neighborhoods), towns, regions, counties, state) %>%
+    purrr::map2(c("Neighborhoods", "Towns", "Regions", "Counties", "State"), function(geo, geo_head) {
+      if (!is.null(geo)) {
+        str1 <- paste(geo, collapse = ", ")
+        paste(geo_head, str1, sep = ": ")
+      }
+    }) %>%
+    purrr::discard(function(x) is.null(x)) %>%
+    as.character()
+  if (msa) {
+    out <- c(out, "MSA: All in New England")
+  }
+  if (us) {
+    out <- c(out, "US: Yes")
+  }
+  paste(out, collapse = "\n")
+}
