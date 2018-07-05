958e0a70aef51b07fadaff259502ea1a136772e9
camille-s <camille.seaberry@gmail.com>
2018-07-05 16:27:06 -0400

    Make neighborhood shares datasets, crosswalks

--- data-raw/make_geo_sf.R
+++ data-raw/make_geo_sf.R
@@ -3,26 +3,25 @@ town_sf <- tigris::county_subdivisions(state = "09", cb = T, class = "sf") %>%
   dplyr::select(name = NAME, GEOID, geometry)
 
 
-new_haven_sf <- geojsonio::geojson_read("https://gist.githubusercontent.com/camille-s/c8cfa583ef22105e90d53ceb299f1a7b/raw/fc087f30ddb2658a05fb5408f1e9d5276b8a433d/nhv.json", what = "sp") %>%
-  sf::st_as_sf() %>%
+new_haven_sf <- sf::st_read("https://gist.githubusercontent.com/camille-s/c8cfa583ef22105e90d53ceb299f1a7b/raw/fc087f30ddb2658a05fb5408f1e9d5276b8a433d/nhv.json") %>%
   dplyr::rename(name = neighborhood) %>%
   dplyr::mutate(name = forcats::fct_recode(name, "Wooster Square" = "Wooster Square/Mill River"))
 
 
-stamford_sf <- geojsonio::geojson_read("https://gist.github.com/camille-s/7002148f77b0020f780f46127be5e9ea/raw/084af721d6dcf509b29c69b3425179fda0748f03/stamford.geojson", what = "sp") %>%
-  sf::st_as_sf() %>%
+stamford_sf <- sf::st_read("https://gist.github.com/camille-s/7002148f77b0020f780f46127be5e9ea/raw/084af721d6dcf509b29c69b3425179fda0748f03/stamford.geojson") %>%
   dplyr::select(name, geometry)
 
 
-bridgeport_sf <- geojsonio::geojson_read("https://gist.github.com/camille-s/c5e4f0178cfbf288af454016018f5173/raw/43faa412db577c4f770d861ab913116ccdb3d445/bpt_tract_neighborhoods.geojson", what = "sp") %>%
-  sf::st_as_sf() %>%
+bridgeport_sf <- sf::st_read("https://gist.github.com/camille-s/c5e4f0178cfbf288af454016018f5173/raw/43faa412db577c4f770d861ab913116ccdb3d445/bpt_tract_neighborhoods.geojson") %>%
   dplyr::select(name = Name, geometry) %>%
-  sf::st_transform(sf::st_crs(new_haven_sf))
+  sf::st_transform(sf::st_crs(tract_sf)) %>%
+  sf::st_simplify(dTolerance = 1e-6)
 
 
-hartford_sf <- geojsonio::geojson_read("https://gist.github.com/camille-s/9e9761b69a7c86bf6d7163cb73636f26/raw/3858f538d955c022b43e168e4c7cec316c2e437f/hfd_shape.json", what = "sp") %>%
-  sf::st_as_sf() %>%
-  dplyr::select(name = Neighborhood, town = Town, geometry)
+hartford_sf <- sf::st_read("https://gist.github.com/camille-s/9e9761b69a7c86bf6d7163cb73636f26/raw/3858f538d955c022b43e168e4c7cec316c2e437f/hfd_shape.json") %>%
+  dplyr::select(name = Neighborhood, town = Town, geometry) %>%
+  sf::st_set_crs(sf::st_crs(tract_sf)) %>%
+  sf::st_transform(sf::st_crs(tract_sf))
 
 
 tract_sf <- tigris::tracts(state = "09", cb = T, class = "sf") %>%
@@ -34,3 +33,4 @@ usethis::use_data(stamford_sf, overwrite = T)
 usethis::use_data(bridgeport_sf, overwrite = T)
 usethis::use_data(hartford_sf, overwrite = T)
 usethis::use_data(tract_sf, overwrite = T)
+
