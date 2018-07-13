fc172fb674689396a61badfbdac734474f4f5b45
camille-s <camille.seaberry@gmail.com>
2018-07-13 16:32:53 -0400

    Add adj_inflation

---
+++ R/adj_inflation.R
@@ -0,0 +1,60 @@
+#' Add inflation-adjusted values to a data frame
+#'
+#' This is a wrapper around [`blscrapeR::bls_api()`] modeled after [`blscrapeR::inflation_adjust()`] that joins a data frame with an inflation adjustment table, then calculates adjusted values. It returns the original data frame with two additional columns for adjustment factors and adjustment values.
+#'
+#' **Note:** Because `adj_inflation` wraps around a function that makes API calls, internet access is required.
+#'
+#' @param data A data frame containing monetary values by year.
+#' @param value Bare column name of monetary values; for safety, has no default.
+#' @param year Bare column name of years; for safety, has no default.
+#' @param base_year Year on which to base inflation amounts; defaults to 2016.
+#' @param key A string giving the BLS API key. Defaults to the value in `Sys.getenv("BLS_KEY")`, as set by `blscrapeR::set_bls_key`.
+#' @return A data frame with two additional columns: adjustment factors, and adjusted values. The adjusted values column is named based on the name supplied as `value`; e.g. if `value = avg_wage`, the adjusted column is named `adj_avg_wage`.
+#' @examples
+#' \dontrun{
+#'   wages <- data.frame(
+#'     year = 2010:2016,
+#'     wage = c(50000, 51000, 52000, 53000, 54000, 55000, 54000)
+#'   )
+#'   adj_inflation(wages, value = wage, year = year, base_year = 2016)
+#' }
+#' @export
+adj_inflation <- function(data, value, year, base_year = 2016, key = Sys.getenv("BLS_KEY")) {
+  if (missing(value) | missing(year)) stop("Both value and year are required.")
+  assertthat::assert_that(curl::has_internet(), msg = "Internet access is required to run this function.")
+
+  value_var <- rlang::enquo(value)
+  year_var <- rlang::enquo(year)
+
+  adj_var <- paste("adj", rlang::quo_name(value_var), sep = "_")
+
+  yr_range <- range(data[rlang::quo_name(year_var)])
+  startyear <- min(c(yr_range[1], base_year))
+  endyear <- max(c(yr_range[2], base_year))
+
+  # API only handles 20 years at a time
+  if (endyear - startyear > 20) {
+    message("The API can only get 20 years of data at once; making multiple calls, but this might take a little longer.")
+  }
+  year_df <- data.frame(years = startyear:endyear)
+  year_df$brk <- floor(year_df$years / 20)
+  years_split <- split(year_df, year_df$brk) %>%
+    purrr::map(dplyr::pull, years)
+
+  cpi_series <- "CUSR0000SA0"
+
+  cpi <- purrr::map_dfr(years_split, function(yrs) {
+    blscrapeR::bls_api(cpi_series, min(yrs), max(yrs), annualaverage = F, registrationKey = key)
+  }) %>%
+    dplyr::select(year, period, value) %>%
+    dplyr::group_by(year) %>%
+    dplyr::summarise(avg_cpi = mean(value)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(adj_factor = round(avg_cpi / avg_cpi[year == base_year], 3)) %>%
+    dplyr::select(-avg_cpi)
+
+  data %>%
+    dplyr::mutate(!!rlang::quo_name(year_var) := as.numeric(!!year_var)) %>%
+    dplyr::left_join(cpi, by = c(rlang::quo_name(year_var), "year")) %>%
+    dplyr::mutate(!!adj_var := !!value_var / adj_factor)
+}
--- R/laus_trend.R
+++ R/laus_trend.R
@@ -14,7 +14,19 @@
 #' }
 #' @export
 laus_trend <- function(towns, startyear, endyear, measures = "all", annual = FALSE, key = Sys.getenv("BLS_KEY")) {
+  # make sure there's an API key
   assertthat::assert_that(!is.null(key), nchar(key) > 0, msg = "A BLS API key is required. Please see blscrapeR::set_bls_key for installation")
+  # BLS API maxes at 20 years--split into groups of 20
+  if (endyear - startyear > 20) {
+    message("The API can only get 20 years of data at once; making multiple calls, but this might take a little longer.")
+  }
+  year_df <- data.frame(years = startyear:endyear)
+  year_df$brk <- floor(year_df$years / 20)
+  years_split <- split(year_df, year_df$brk) %>%
+    purrr::map(dplyr::pull, years)
+
+  # make sure measures isn't something weird & invalid
+  # measures lookup table
   if (identical(measures, "all")) {
     measure_lookup <- laus_measures
   } else {
@@ -22,16 +34,26 @@ laus_trend <- function(towns, startyear, endyear, measures = "all", annual = FAL
     measure_lookup <- dplyr::data_frame(measure_text = measures) %>%
       dplyr::inner_join(laus_measures, by = "measure_text")
   }
+  # laus area lookup table
   assertthat::assert_that(all(towns %in% laus_codes$area), msg = "Limit towns to valid Connecticut town and county names")
   areas <- laus_codes %>%
     dplyr::filter(area %in% towns)
 
+  # to make series IDs, get all combinations of measure codes and area codes
   series_df <- tidyr::crossing(measure_lookup, areas) %>%
     dplyr::select(area, area_code = code, measure_text, measure_code) %>%
     dplyr::mutate(series = sprintf("LAU%s%s", area_code, measure_code)) %>%
     dplyr::select(-area_code, -measure_code)
 
-  blscrapeR::bls_api(seriesid = series_df$series, startyear = startyear, endyear = endyear, registrationKey = key, annualaverage = annual) %>%
-    dplyr::left_join(series_df, by = c("seriesID" = "series")) %>%
-    dplyr::select(measure_text, area, year, period, periodName, value, seriesID, footnotes)
+  # map over years_split, make calls for each
+  out <- purrr::map_dfr(years_split, function(yrs) {
+    y1 <- min(yrs)
+    y2 <- max(yrs)
+    blscrapeR::bls_api(seriesid = series_df$series, startyear = y1, endyear = y2, registrationKey = key, annualaverage = annual) %>%
+      dplyr::left_join(series_df, by = c("seriesID" = "series")) %>%
+      dplyr::select(measure_text, area, year, period, periodName, value, seriesID, footnotes)
+  })
+
+  out
 }
+
