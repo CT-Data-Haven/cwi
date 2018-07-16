869cd7c1168a411d97e1a0106ada826925966da2
camille-s <camille.seaberry@gmail.com>
2018-07-16 12:07:55 -0400

    Add decennial vars and table number dfs

--- data-raw/make_acs_vars.R
+++ data-raw/make_acs_vars.R
@@ -4,3 +4,15 @@ acs_vars <- tidycensus::load_variables(year = 2016, "acs5", cache = T) %>%
   dplyr::mutate(name = stringr::str_remove(name, "E$"))
 
 usethis::use_data(acs_vars, overwrite = T)
+
+decennial_vars <- tidycensus::load_variables(year = 2010, "sf1", cache = T) %>%
+  dplyr::filter(stringr::str_detect(name, "^(H|P|HCT|PCO|PCT)\\d+"))
+
+decennial_nums <- decennial_vars %>%
+  dplyr::filter(!stringr::str_detect(name, "^H0001")) %>%
+  dplyr::pull(name) %>%
+  stringr::str_extract("^(H|P|HCT|PCT|PCO)\\d{3}") %>%
+  unique()
+
+usethis::use_data(decennial_vars, overwrite = T)
+usethis::use_data(decennial_nums, overwrite = T, internal = T)
