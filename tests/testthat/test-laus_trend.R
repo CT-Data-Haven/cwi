21417fdcf6a91f09df5ff8076f04c1d82714a977
camille-s <camille.seaberry@gmail.com>
2018-07-20 10:14:15 -0400

    Add travis skips in tests

--- tests/testthat/test-laus_trend.R
+++ tests/testthat/test-laus_trend.R
@@ -3,22 +3,26 @@ library(cwi)
 library(testthat)
 
 test_that("checks for API key", {
-  # expect_error(laus_trend("New Haven", 2012, 2016, key = NULL), "API key is required")
+  skip_on_travis()
+  expect_error(laus_trend("New Haven", 2012, 2016, key = NULL), "API key is required")
   # such as if not set in .Renviron
-  # expect_error(laus_trend("New Haven", 2012, 2016, key = ""), "API key is required")
+  expect_error(laus_trend("New Haven", 2012, 2016, key = ""), "API key is required")
 })
 
 test_that("checks possible measures & accepts keyword 'all'", {
-  # expect_error(laus_trend("New Haven", 2012, 2016, measures = "jobs"), "Possible measures are")
+  skip_on_travis()
+  expect_error(laus_trend("New Haven", 2012, 2016, measures = "jobs"), "Possible measures are")
 
-  # expect_equal(nrow(laus_trend("New Haven", 2016, 2016, measures = "all", annual = F)), 12 * 4)
+  expect_equal(nrow(laus_trend("New Haven", 2016, 2016, measures = "all", annual = F)), 12 * 4)
 })
 
 test_that("catches invalid area names", {
-  # expect_error(laus_trend(c("New Haven", "South Haven"), 2016, 2016), "Limit names")
+  skip_on_travis()
+  expect_error(laus_trend(c("New Haven", "South Haven"), 2016, 2016), "Limit names")
 })
 
 test_that("handles more than 20 years okay", {
-  # expect_message(laus_trend("New Haven", 1990, 2016, measures = "employment"), "multiple calls")
-  # expect_equal(nrow(laus_trend("New Haven", 1990, 2016, measures = "employment")), 27 * 12)
+  skip_on_travis()
+  expect_message(laus_trend("New Haven", 1990, 2016, measures = "employment"), "multiple calls")
+  expect_equal(nrow(laus_trend("New Haven", 1990, 2016, measures = "employment")), 27 * 12)
 })
