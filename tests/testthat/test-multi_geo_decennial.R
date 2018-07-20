9d6010f182c08cc76484a59532eb340a1b460c6b
camille-s <camille.seaberry@gmail.com>
2018-07-19 21:11:22 -0400

    Try taking out most API-based tests

--- tests/testthat/test-multi_geo_decennial.R
+++ tests/testthat/test-multi_geo_decennial.R
@@ -4,21 +4,21 @@ library(testthat)
 library(dplyr)
 
 test_that("state names and FIPS codes are validated", {
-  expect_error(multi_geo_decennial(table = "P035", state = NULL), "Must supply")
-  expect_message(multi_geo_decennial(table = "P035", state = 9), "Converting")
-  expect_error(multi_geo_decennial(table = "P035", state = "New Haven"), "valid state name")
+  # expect_error(multi_geo_decennial(table = "P035", state = NULL), "Must supply")
+  # expect_message(multi_geo_decennial(table = "P035", state = 9), "Converting")
+  # expect_error(multi_geo_decennial(table = "P035", state = "New Haven"), "valid state name")
 })
 
 test_that("counties are validated", {
-  expect_warning(multi_geo_decennial(table = "P035", state = "09", counties = c("New Haven County", "Cook County")))
-  expect_warning(multi_geo_decennial(table = "P035", state = "09", counties = c("New Haven County", "Cook County")), "Cook")
+  # expect_warning(multi_geo_decennial(table = "P035", state = "09", counties = c("New Haven County", "Cook County")))
+  # expect_warning(multi_geo_decennial(table = "P035", state = "09", counties = c("New Haven County", "Cook County")), "Cook")
 })
 
 test_that("messages from get_decennial are suppressed if requested", {
-  expect_silent(multi_geo_decennial(table = "P035", towns = NULL, counties = NULL, verbose = F))
-  expect_message(multi_geo_decennial(table = "P035", towns = NULL, counties = NULL, verbose = T))
+  # expect_silent(multi_geo_decennial(table = "P035", towns = NULL, counties = NULL, verbose = F))
+  # expect_message(multi_geo_decennial(table = "P035", towns = NULL, counties = NULL, verbose = T))
 })
 
 test_that("invalid table numbers throw error with formatted number", {
-  expect_error(multi_geo_decennial(table = "P35", towns = NULL, counties = NULL))
+  # expect_error(multi_geo_decennial(table = "P35", towns = NULL, counties = NULL))
 })
