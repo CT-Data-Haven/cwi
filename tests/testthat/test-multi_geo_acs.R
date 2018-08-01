2a5c47fb19483af78e5a8921ce8a31a2b190a76d
camille-s <camille.seaberry@gmail.com>
2018-08-01 17:49:11 -0400

    Test survey arg in multi_geo_acs

--- tests/testthat/test-multi_geo_acs.R
+++ tests/testthat/test-multi_geo_acs.R
@@ -22,3 +22,10 @@ test_that("messages from get_acs are suppressed if requested", {
   expect_silent(multi_geo_acs(table = "B01003", towns = NULL, counties = NULL, verbose = F))
   expect_message(multi_geo_acs(table = "B01003", towns = NULL, counties = NULL, verbose = T))
 })
+
+test_that("handles 1-year and 3-year surveys", {
+  # should add warnings for when 1- and 3-year aren't available
+  skip_on_travis()
+  expect_equal(nrow(multi_geo_acs(table = "B01003", state = "09", towns = NULL, survey = "acs1", year = 2016)), 9)
+  expect_equal(nrow(multi_geo_acs(table = "B01003", state = "09", towns = NULL, survey = "acs3", year = 2013)), 9)
+})
