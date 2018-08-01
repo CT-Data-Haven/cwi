31de5269acae133bc57b4e57575336860af2c18e
camille-s <camille.seaberry@gmail.com>
2018-08-01 14:57:38 -0400

    Fix acs_vars, decennial_vars to account for year

--- R/label_acs.R
+++ R/label_acs.R
@@ -1,14 +1,30 @@
+# get acs variables by year, cached
+clean_acs_vars <- function(year) {
+  tidycensus::load_variables(year = year, "acs5", cache = T) %>%
+    dplyr::filter(stringr::str_detect(name, "_\\d{3}E$")) %>%
+    dplyr::mutate(label = stringr::str_remove(label, "Estimate!!")) %>%
+    dplyr::mutate(name = stringr::str_remove(name, "E$"))
+}
+
+# get decennial vars by year, cached
+clean_decennial_vars <- function(year) {
+  tidycensus::load_variables(year = year, "sf1", cache = T) %>%
+    dplyr::filter(stringr::str_detect(name, "^(H|P|HCT|PCO|PCT)\\d+"))
+}
+
 #' Quickly add the labels of ACS variables
 #'
 #' `tidycensus::get_acs` returns an ACS table with its variable codes, which can be joined with `cwi::acs_vars` to get readable labels. This function is just a quick wrapper around the common task of joining these two data frames.
 #' @param df A data frame/tibble.
-#' @param variable The bare column name of variable codes; defaults to `variable`, as returned by `get_acs`.
+#' @param year The endyear of ACS data; defaults 2016.
+#' @param variable The bare column name of variable codes; defaults to `variable`, as returned by `tidycensus::get_acs`.
 #' @return A tibble
 #' @seealso [acs_vars]
 #' @export
-label_acs <- function(df, variable = variable) {
+label_acs <- function(df, year = 2016, variable = variable) {
   variable_var <- rlang::enquo(variable)
   variable_name <- rlang::quo_name(variable_var)
+  acs_vars <- clean_acs_vars(year = year)
   df %>%
     dplyr::left_join(acs_vars %>% dplyr::select(-concept), by = stats::setNames("name", variable_name))
 }
--- data-raw/make_acs_vars.R
+++ data-raw/make_acs_vars.R
@@ -1,18 +1,14 @@
-acs_vars <- tidycensus::load_variables(year = 2016, "acs5", cache = T) %>%
-  dplyr::filter(stringr::str_detect(name, "_\\d{3}E$")) %>%
-  dplyr::mutate(label = stringr::str_remove(label, "Estimate!!")) %>%
-  dplyr::mutate(name = stringr::str_remove(name, "E$"))
+acs_vars16 <- clean_acs_vars(year = 2016)
 
-usethis::use_data(acs_vars, overwrite = T)
+usethis::use_data(acs_vars16, overwrite = T)
 
-decennial_vars <- tidycensus::load_variables(year = 2010, "sf1", cache = T) %>%
-  dplyr::filter(stringr::str_detect(name, "^(H|P|HCT|PCO|PCT)\\d+"))
+decennial_vars10 <- clean_decennial_vars(year = 2010)
 
-decennial_nums <- decennial_vars %>%
+decennial_nums <- decennial_vars10 %>%
   dplyr::filter(!stringr::str_detect(name, "^H0001")) %>%
   dplyr::pull(name) %>%
   stringr::str_extract("^(H|P|HCT|PCT|PCO)\\d{3}[A-Z]?") %>%
   unique()
 
-usethis::use_data(decennial_vars, overwrite = T)
+usethis::use_data(decennial_vars10, overwrite = T)
 
