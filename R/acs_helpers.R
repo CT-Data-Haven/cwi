ae096fc8342490d43090214f55f9b363812e72ea
camille-s <camille.seaberry@gmail.com>
2018-08-01 17:43:12 -0400

    Add survey argument to multi_geo_acs

--- R/acs_helpers.R
+++ R/acs_helpers.R
@@ -1,13 +1,13 @@
-acs_towns <- function(table, year, towns, counties, state) {
+acs_towns <- function(table, year, towns, counties, state, survey) {
   if (!is.null(counties) & !identical(counties, "all")) {
     fetch <- counties %>%
       purrr::map_dfr(function(county) {
-        suppressMessages(tidycensus::get_acs(geography = "county subdivision", table = table, year = year, state = state, county = county)) %>%
+        suppressMessages(tidycensus::get_acs(geography = "county subdivision", table = table, year = year, state = state, county = county, survey = survey)) %>%
           dplyr::mutate(county = county)
       }) %>%
       camiller::town_names(NAME)
   } else {
-    fetch <- suppressMessages(tidycensus::get_acs(geography = "county subdivision", table = table, year = year, state = state)) %>%
+    fetch <- suppressMessages(tidycensus::get_acs(geography = "county subdivision", table = table, year = year, state = state, survey = survey)) %>%
       # dplyr::mutate(state = state) %>%
       camiller::town_names(NAME)
   }
@@ -19,8 +19,8 @@ acs_towns <- function(table, year, towns, counties, state) {
     dplyr::mutate(state = state)
 }
 
-acs_counties <- function(table, year, counties, state) {
-  fetch <- suppressMessages(tidycensus::get_acs(geography = "county", table = table, year = year, state = state)) %>%
+acs_counties <- function(table, year, counties, state, survey) {
+  fetch <- suppressMessages(tidycensus::get_acs(geography = "county", table = table, year = year, state = state, survey = survey)) %>%
     dplyr::mutate(NAME = stringr::str_extract(NAME, "^.+County(?=, )")) %>%
     dplyr::mutate(state = state)
 
@@ -30,14 +30,14 @@ acs_counties <- function(table, year, counties, state) {
   fetch
 }
 
-acs_state <- function(table, year, state) {
-  fetch <- suppressMessages(tidycensus::get_acs(geography = "state", table = table, year = year)) %>%
+acs_state <- function(table, year, state, survey) {
+  fetch <- suppressMessages(tidycensus::get_acs(geography = "state", table = table, year = year, survey = survey)) %>%
     dplyr::filter(NAME == state | GEOID == state)
   fetch
 }
 
-acs_regions <- function(table, year, regions, state) {
-  fetch_towns <- suppressMessages(tidycensus::get_acs(geography = "county subdivision", table = table, year = year, state = state)) %>%
+acs_regions <- function(table, year, regions, state, survey) {
+  fetch_towns <- suppressMessages(tidycensus::get_acs(geography = "county subdivision", table = table, year = year, state = state, survey = survey)) %>%
     camiller::town_names(NAME)
 
   regions %>%
@@ -49,8 +49,8 @@ acs_regions <- function(table, year, regions, state) {
     })
 }
 
-acs_neighborhoods <- function(table, year, neighborhoods, state) {
-  fetch_tracts <- suppressMessages(tidycensus::get_acs(geography = "tract", table = table, year = year, state = state))
+acs_neighborhoods <- function(table, year, neighborhoods, state, survey) {
+  fetch_tracts <- suppressMessages(tidycensus::get_acs(geography = "tract", table = table, year = year, state = state, survey = survey))
 
   neighborhoods %>%
     purrr::imap_dfr(function(neighborhood, neighborhood_name) {
@@ -61,8 +61,8 @@ acs_neighborhoods <- function(table, year, neighborhoods, state) {
     })
 }
 
-acs_msa <- function(table, year, new_england) {
-  fetch <- suppressMessages(tidycensus::get_acs(geography = "metropolitan statistical area/micropolitan statistical area", table = table, year = year))
+acs_msa <- function(table, year, new_england, survey) {
+  fetch <- suppressMessages(tidycensus::get_acs(geography = "metropolitan statistical area/micropolitan statistical area", table = table, year = year, survey = survey))
   if (new_england) {
     ne_geoid <- msa %>%
       dplyr::filter(region == "New England") %>%
