7c71b8b0363f3a4dc2c17ec3ae57d52177927427
camille-s <camille.seaberry@gmail.com>
2018-08-06 17:04:31 -0400

    Fix error with P0120026 decennial label

--- data-raw/make_acs_vars.R
+++ data-raw/make_acs_vars.R
@@ -2,7 +2,10 @@ acs_vars16 <- clean_acs_vars(year = 2016, survey = "acs5")
 
 usethis::use_data(acs_vars16, overwrite = T)
 
-decennial_vars10 <- clean_decennial_vars(year = 2010, sumfile = "sf1")
+# there's an error in P012, sex by age, where female, all ages is labeled as female, 85 and up
+
+decennial_vars10 <- clean_decennial_vars(year = 2010, sumfile = "sf1") %>%
+  mutate(label = ifelse(str_detect(name, "^P012\\D?0026"), "Female:", label))
 
 decennial_nums <- decennial_vars10 %>%
   dplyr::filter(!stringr::str_detect(name, "^H0001")) %>%
@@ -11,4 +14,3 @@ decennial_nums <- decennial_vars10 %>%
   unique()
 
 usethis::use_data(decennial_vars10, overwrite = T)
-
