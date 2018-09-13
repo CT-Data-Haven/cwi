810cb2d7b666c6c69a1ffc5f0ac62c40fe9ac077
camille-s <camille.seaberry@gmail.com>
2018-09-13 16:12:17 -0400

    Fix scientific notation in nhv_tracts tract fips

--- data-raw/make_nhv_tracts.R
+++ data-raw/make_nhv_tracts.R
@@ -1,4 +1,6 @@
+# need workaround for cases where tracts were written in scientific notation
 nhv_tracts <- readr::read_csv("./data-raw/files/nhv_neighborhood_tracts.csv", col_types = "ccdc") %>%
-  dplyr::select(name = neighborhood, geoid, tract, weight)
+  dplyr::select(name = neighborhood, geoid, tract, weight) %>%
+  dplyr::mutate(tract = as.character(as.numeric(tract)))
 
 usethis::use_data(nhv_tracts, overwrite = T)
