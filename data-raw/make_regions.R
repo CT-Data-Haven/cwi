2746f045e5c59cc60f6a19e8735b1dbd54571bfa
camille-s <camille.seaberry@gmail.com>
2019-06-18 15:01:29 -0400

    Sort regions by town name

--- data-raw/make_regions.R
+++ data-raw/make_regions.R
@@ -2,6 +2,7 @@ not_split <- readr::read_csv("./data-raw/files/town_region_lookup.csv") %>%
   dplyr::filter(!is.na(region))
 
 regions <- split(not_split, not_split$region) %>%
-  purrr::map(dplyr::pull, town)
+  purrr::map(dplyr::pull, town) %>%
+  purrr::map(sort)
 
 usethis::use_data(regions, overwrite = T)
