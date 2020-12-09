0548729f0b24f2be872de1e9b5bbc06bec282967
camille-s <camille.seaberry@gmail.com>
2020-12-09 18:51:58 -0500

    Don't need to print every neighborhood

--- R/geo_printout.R
+++ R/geo_printout.R
@@ -4,7 +4,11 @@ geo_printout <- function(neighborhoods, towns, regions, counties, state, msa = F
   out <- list(unique(neighborhoods), towns, names(regions), counties, state) %>%
     purrr::map2(c("Neighborhoods", "Towns", "Regions", "Counties", "State"), function(geo, geo_head) {
       if (!is.null(geo)) {
-        str1 <- paste(geo, collapse = ", ")
+        if (geo_head == "Neighborhoods") {
+          str1 <- paste(length(geo), "neighborhoods")
+        } else {
+          str1 <- paste(geo, collapse = ", ")
+        }
         paste(geo_head, str1, sep = ": ")
       }
     }) %>%
