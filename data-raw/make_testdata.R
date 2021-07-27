cf9d77b5156b9df85c883227827829708d1d7008
camille-s <camille.seaberry@gmail.com>
2021-07-27 17:15:04 -0400

    Add separate_acs with tests

---
+++ data-raw/make_testdata.R
@@ -0,0 +1,7 @@
+# make a set of data for testing separate_acs
+# uses census api & doesn't need to run each time testing is done
+age_df <- tidycensus::get_acs("county", table = "B01001", state = "09") %>%
+  label_acs() %>%
+  dplyr::select(geoid = GEOID, label) %>%
+  dplyr::slice(1:10)
+saveRDS(age_df, system.file("test_data/age_df.rds", package = "cwi"))
---
+++ tests/testthat/test-separate_acs.R
@@ -0,0 +1,22 @@
+context("Column numbers in separate_acs")
+library(cwi)
+library(testthat)
+
+test_that("handles column specifications", {
+  df <- readRDS(system.file("test_data/age_df.rds", package = "cwi"))
+
+  expect_equal(names(suppressWarnings(separate_acs(df))), c("geoid", "x1", "x2", "x3"))
+  expect_length(suppressWarnings(separate_acs(df)), 4)
+  expect_length(suppressWarnings(separate_acs(df, drop_total = TRUE)), 3)
+  expect_length(suppressWarnings(separate_acs(df, into = c("total", "sex", "age"))), 4)
+  expect_length(suppressWarnings(separate_acs(df, into = c("sex", "age"), drop_total = TRUE)), 3)
+  expect_identical(suppressWarnings(separate_acs(df, into = c("sex", "age"), drop_total = TRUE)),
+                   suppressWarnings(separate_acs(df, into = c(NA, "sex", "age"))))
+})
+
+test_that("passes arguments to tidyr::separate", {
+  df <- readRDS(system.file("test_data/age_df.rds", package = "cwi"))
+  expect_warning(separate_acs(df))
+  expect_silent(separate_acs(df, fill = "left"))
+  expect_true(any(is.na(suppressWarnings(separate_acs(df, fill = "left"))$x1)))
+})
