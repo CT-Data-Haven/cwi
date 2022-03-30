84004255d8e00b002eefbaf349b09fc8c3cd080a
camille-s <camille.seaberry@gmail.com>
2022-03-30 19:18:17 -0400

    Add occupation codes dataset

--- data-raw/make_lehd.R
+++ data-raw/make_lehd.R
@@ -1,9 +1,11 @@
+# INDUSTRY CODES -- NAICS
 naics_codes <- readr::read_csv("https://lehd.ces.census.gov/data/schema/latest/label_industry.csv", col_types = "ccc") %>%
   dplyr::filter(industry == "00" | ind_level == "2")
 
 usethis::use_data(naics_codes, overwrite = TRUE)
 
-# loading status: years available in api by state
+
+# QWI AVAILABILITY -- YEARS AVAILABLE BY STATE
 qwi_avail <- rvest::read_html("https://ledextract.ces.census.gov/loading_status.html") %>%
   rvest::html_table() %>%
   `[[`(1) %>%
@@ -13,3 +15,37 @@ qwi_avail <- rvest::read_html("https://ledextract.ces.census.gov/loading_status.
   dplyr::select(state_code, start_year = start_quarter, end_year = end_quarter)
 
 usethis::use_data(qwi_avail, overwrite = TRUE)
+
+
+# OCCUPATION CODES -- OCC (CENSUS) & SOC (BLS)
+# keep most groups top-level except management etc (first category)
+occ_fn <- file.path("data-raw", "files", "occ_codes.xlsx")
+if (!file.exists(occ_fn)) {
+  download.file(url = "https://www2.census.gov/programs-surveys/demo/guidance/industry-occupation/2018-ACS-PUMS-and-2018-SIPP-Public-Use-Occupation-Code-List.xlsx",
+                destfile = occ_fn)
+}
+top_occ <- readxl::read_excel(occ_fn, sheet = 1, range = "C4:C8", col_names = "description") %>%
+  tidyr::extract(description, into = c("occ_code", "description"), regex = "(\\d{4}\\s\\-\\s\\d{4})\\s+([\\w\\s[:punct:]]+)$") %>%
+  dplyr::mutate(occ_code = gsub("\\s", "", occ_code)) %>%
+  dplyr::mutate(is_top = TRUE)
+
+# headings list a range of codes in occ_codes
+occ <- readxl::read_excel(occ_fn, sheet = 1, skip = 10, col_names = c("occ_code", "soc_code", "description")) %>%
+  dplyr::mutate(is_hdr = grepl("\\-", occ_code)) %>%
+  dplyr::filter(!grepl("Combines", soc_code),
+                !is.na(soc_code),
+                !is.na(occ_code)) %>%
+  dplyr::mutate(description = stringr::str_squish(description)) %>%
+  dplyr::mutate(description = gsub("\\:$", "", description))
+
+occ_codes <- occ %>%
+  dplyr::left_join(top_occ, by = c("occ_code", "description")) %>%
+  dplyr::filter(is_hdr) %>%
+  dplyr::mutate(is_subhdr = is.na(is_top) & (grepl("\\s\\-\\s", soc_code) | grepl("Military Specific", description))) %>%
+  dplyr::mutate(is_major_grp = tidyr::replace_na((is_top | is_subhdr), FALSE)) %>%
+  dplyr::mutate(occ_group = ifelse(is_major_grp, description, NA_character_)) %>%
+  dplyr::filter(description != "Management, Business, Science, and Arts Occupations") %>%
+  tidyr::fill(occ_group, .direction = "down") %>%
+  dplyr::select(is_major_grp, occ_group, occ_code, soc_code, description)
+
+usethis::use_data(occ_codes, overwrite = TRUE)
